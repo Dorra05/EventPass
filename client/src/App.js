9a61df0f448a771c63ecff769d0bb081fa52ac2e
JavaScript_Project_2 <douda0512@yahoo.fr>
2025-04-30 18:34:51 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Paiement from "./components/Paiement";
+import AddingTicket from "./components/AddingTicket";
+import ListofTickets from "./components/ListofTickets";
+import OneUser from "./components/OneUser";
+import ListOfUsers from "./components/ListOfUsers";
+import AddUser from "./components/AddUser";
+import ListOfEvents from "./components/ListofEvents";
+import AddEvent from "./components/AddEvent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bienvenue sur EventPass</h1>
-        <p>Votre plateforme de billetterie en ligne</p>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="login" element={<Login />} />
+          <Route path="paiement/:id" element={<Paiement />} />
+          <Route path="listoftickets" element={<ListofTickets />} />
+          <Route path="addTicket" element={<AddingTicket />} />
+          <Route path="OneUser" element={<OneUser />} />
+          <Route path="ListOfUsers" element={<ListOfUsers />} />
+          <Route path="addUser" element={<AddUser />} />
+          <Route path="/events" element={<ListOfEvents />} />
+          <Route path="/add-event" element={<AddEvent />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
